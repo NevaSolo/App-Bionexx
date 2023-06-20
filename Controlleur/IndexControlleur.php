598e5560ab0f18aadab9ae97e4e7e4a0355cc829
NevaSolo <136957475+NevaSolo@users.noreply.github.com>
2023-06-20 15:49:40 +0300

    Update IndexControlleur.php

--- Controlleur/IndexControlleur.php
+++ Controlleur/IndexControlleur.php
@@ -1,45 +1,45 @@
 <?php
 //Directeur
-if(isset($_POST['dirco'])){
-    extract($_POST);
+if(isset($_GET['dirco'])){
+    extract($_GET);
     header("Location: ../Vue/Directeur/Directeur.php");
     
 }
-if(isset($_POST['dirin'])){
-    extract($_POST);
+if(isset($_GET['dirin'])){
+    extract($_GET);
     header("Location: ../Vue/Directeur/Enregistrement.php");
 }
 
 //Admin
-if(isset($_POST['adco'])){
-    extract($_POST);
+if(isset($_GET['adco'])){
+    extract($_GET);
     header("Location: ../Vue/Administrateur/Admin.php");
     
 }
-if(isset($_POST['adin'])){
-    extract($_POST);
+if(isset($_GET['adin'])){
+    extract($_GET);
     header("Location: ../Vue/Administrateur/Enregistrement.php");
 }
 
 //chef
-if(isset($_POST['chefco'])){
-    extract($_POST);
+if(isset($_GET['chefco'])){
+    extract($_GET);
     header("Location: ../Vue/Chef_departement/Chef.php");
     
 }
-if(isset($_POST['chefin'])){
-    extract($_POST);
+if(isset($_GET['chefin'])){
+    extract($_GET);
     header("Location: ../Vue/Chef_departement/Enregistrement.php");
 }
 
 //Utilisateur
-if(isset($_POST['utco'])){
-    extract($_POST);
+if(isset($_GET['utco'])){
+    extract($_GET);
     header("Location: ../Vue/Utilisateur/Login.php");
     
 }
-if(isset($_POST['utin'])){
-    extract($_POST);
+if(isset($_GET['utin'])){
+    extract($_GET);
     header("Location: ../Vue/Utilisateur/Enregistrement.php");
 }
 ?>
